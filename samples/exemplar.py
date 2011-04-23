4deba567d80298eb81aace5a134db023ee309d37
Ethan Rublee <ethan.rublee@gmail.com>
2011-04-22 19:38:33 -0700

    exemplar.

--- samples/exemplar.py
+++ samples/exemplar.py
@@ -29,10 +29,11 @@ class SobelAbs(ecto.Module):
         self.sobel = imgproc.Sobel(x = self.params.x, y = self.params.y)
         self.abs = imgproc.AbsNormalized()
         
+        
         self.sobel.inputs["input"].connect(self.inputs["input"])
         self.outputs["out"].connect(self.abs.outputs["out"])
         self.outputs["gradient"].connect(self.sobel.outputs["out"])
-        
+                
         self.plasm.connect(self.sobel,"out",self.abs,"input")   
 
         if debug:
@@ -43,54 +44,79 @@ class SobelAbs(ecto.Module):
         plasm = self.plasm
         sobel = self.sobel
         abs = self.abs
-                
         plasm.mark_dirty(sobel) #mark the sobel dirty
         plasm.go(abs)
 
+
+class SobelXY(ecto.Module):
+    def __init__(self, **kwargs): 
+        #call init, which will thunk to Params, then to config
+        ecto.Module.__init__(self,**kwargs)
+        
+    @staticmethod
+    def Params(params):
+        pass
+
+    def config(self):
+        #declare inputs/outputs, these are exposed to outside world
+        self.inputs.declare("input","an image", None)
+        self.outputs.declare("out", "an sobel XY image", None)
+        self.outputs.declare("x", "an sobel X image", None)
+        self.outputs.declare("y", "an sobel Y image", None)
+
+        
+        #setup internal plasm here:
+        plasm = ecto.Plasm()
+        sobelX = SobelAbs( x= 1, y = 0)
+        sobelY = SobelAbs( x= 0, y = 1)
+        adder = imgproc.ImageAdder()
+
+        plasm.connect(sobelX, "out", adder , "a")
+        plasm.connect(sobelY, "out", adder , "b")
+
+        #print plasm.viz()
+        ecto.view_plasm(plasm)
         
         
-video = highgui.VideoCapture()
-imshow = highgui.imshow(name="video",waitKey=2)
-sobelShower = highgui.imshow(name="sobel",waitKey=-1)
-grayShower = highgui.imshow(name="gray",waitKey=-1)
-gradientShow = highgui.imshow(name="gradient",waitKey=-1)
+        #tie the inputs to the outputs
+        sobelX.inputs["input"].connect(self.inputs["input"])
+        sobelY.inputs["input"].connect(self.inputs["input"])
+        self.outputs["out"].connect(adder.outputs["out"])
+        self.outputs["x"].connect(sobelX.outputs["out"])
+        self.outputs["y"].connect(sobelY.outputs["out"])
 
-sobelX = SobelAbs( x= 1, y = 0)
-sobelY = SobelAbs( x= 0, y = 1)
+        self.plasm = plasm
+        self.input_modules = (sobelX,sobelY)
+        self.output_modules = (adder,)
+        
+        if debug:
+            ecto.view_plasm(self.plasm)
 
-rgb2gray = imgproc.cvtColor(flag=7)
-
-adder = imgproc.ImageAdder()
-
-plasm = ecto.Plasm()
-plasm.connect(video, "out", imshow, "input")
-plasm.connect(video, "out", rgb2gray , "input")
-plasm.connect(rgb2gray, "out", sobelX, "input")
-plasm.connect(rgb2gray, "out", sobelY, "input")
-plasm.connect(sobelX, "out", adder , "a")
-plasm.connect(sobelY, "out", adder , "b")
-plasm.connect(rgb2gray, "out", grayShower,"input")
-plasm.connect(adder, "out", sobelShower, "input")
-plasm.connect(sobelX, "gradient", gradientShow, "input")
-
-#print plasm.viz()
-ecto.view_plasm(plasm)
-
-while(imshow.outputs.out != 27):
-    plasm.mark_dirty(video)
-    # TODO just call go on the whole plasm, to trigger all leaves being called. 
-    plasm.go(sobelShower)
-    plasm.go(grayShower)
-    plasm.go(imshow)
-    plasm.go(gradientShow)
+    def process(self):
+        for input in self.input_modules:
+            self.plasm.mark_dirty(input)
+        for output in self.output_modules:
+            self.plasm.go(output)
 
 
-#def test_python_module():
-#    mod = Identity(factor = 5.3)
-#    mod.inputs.input = 10
-#    mod.process()
-#    print mod.outputs.out
-#    assert mod.outputs.out == 10
-#
-#if __name__ == '__main__':
-#    test_python_module()
+def video_test():
+    video = highgui.VideoCapture()
+    imshow = highgui.imshow(name="video",waitKey=2)
+    sXY = SobelXY()
+    sobelshow = highgui.imshow(name="sobel",waitKey=-1)
+
+    plasm = ecto.Plasm()
+    plasm.connect(video, "out", imshow, "input")
+    plasm.connect(video, "out", sXY, "input")
+    plasm.connect(sXY, "out", sobelshow, "input")
+
+    ecto.view_plasm(plasm)
+
+    while(imshow.outputs.out != 27):
+        plasm.mark_dirty(video)
+        # TODO just call go on the whole plasm, to trigger all leaves being called. 
+        plasm.go(sobelshow)
+        plasm.go(imshow)
+
+if __name__ == '__main__':
+    video_test()
