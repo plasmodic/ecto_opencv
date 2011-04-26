8051ca638e5a784d820102f166b45cdd95cc77ef
Ethan Rublee <ethan.rublee@gmail.com>
2011-04-26 09:17:51 -0700

    working on examplar.

--- samples/exemplar.py
+++ samples/exemplar.py
@@ -10,7 +10,7 @@ debug = False
 class SobelAbs(ecto.Module):
     def __init__(self, **kwargs): 
         #call init, which will thunk to Params, then to config
-        ecto.Module.__init__(self,**kwargs)
+        ecto.Module.__init__(self, **kwargs)
         
     @staticmethod
     def Params(params):
@@ -20,13 +20,13 @@ class SobelAbs(ecto.Module):
 
     def config(self):
         #declare inputs/outputs, these are exposed to outside world
-        self.inputs.declare("input","an image", None)
+        self.inputs.declare("input", "an image", None)
         self.outputs.declare("gradient", "a gradient image", None)
         self.outputs.declare("out", "an absolute gradient image", None)
         
         #setup internal plasm here:
         self.plasm = ecto.Plasm()
-        self.sobel = imgproc.Sobel(x = self.params.x, y = self.params.y)
+        self.sobel = imgproc.Sobel(x=self.params.x, y=self.params.y)
         self.abs = imgproc.AbsNormalized()
         
         
@@ -34,7 +34,7 @@ class SobelAbs(ecto.Module):
         self.outputs["out"].connect(self.abs.outputs["out"])
         self.outputs["gradient"].connect(self.sobel.outputs["out"])
                 
-        self.plasm.connect(self.sobel,"out",self.abs,"input")   
+        self.plasm.connect(self.sobel, "out", self.abs, "input")   
 
         if debug:
             ecto.view_plasm(self.plasm)
@@ -51,7 +51,7 @@ class SobelAbs(ecto.Module):
 class SobelXY(ecto.Module):
     def __init__(self, **kwargs): 
         #call init, which will thunk to Params, then to config
-        ecto.Module.__init__(self,**kwargs)
+        ecto.Module.__init__(self, **kwargs)
         
     @staticmethod
     def Params(params):
@@ -59,25 +59,20 @@ class SobelXY(ecto.Module):
 
     def config(self):
         #declare inputs/outputs, these are exposed to outside world
-        self.inputs.declare("input","an image", None)
+        self.inputs.declare("input", "an image", None)
         self.outputs.declare("out", "an sobel XY image", None)
         self.outputs.declare("x", "an sobel X image", None)
         self.outputs.declare("y", "an sobel Y image", None)
-
         
         #setup internal plasm here:
         plasm = ecto.Plasm()
-        sobelX = SobelAbs( x= 1, y = 0)
-        sobelY = SobelAbs( x= 0, y = 1)
+        sobelX = SobelAbs(x=1, y=0)
+        sobelY = SobelAbs(x=0, y=1)
         adder = imgproc.ImageAdder()
 
         plasm.connect(sobelX, "out", adder , "a")
         plasm.connect(sobelY, "out", adder , "b")
-
-        #print plasm.viz()
-        ecto.view_plasm(plasm)
-        
-        
+     
         #tie the inputs to the outputs
         sobelX.inputs["input"].connect(self.inputs["input"])
         sobelY.inputs["input"].connect(self.inputs["input"])
@@ -86,7 +81,7 @@ class SobelXY(ecto.Module):
         self.outputs["y"].connect(sobelY.outputs["out"])
 
         self.plasm = plasm
-        self.input_modules = (sobelX,sobelY)
+        self.input_modules = (sobelX, sobelY)
         self.output_modules = (adder,)
         
         if debug:
@@ -101,16 +96,17 @@ class SobelXY(ecto.Module):
 
 def video_test():
     video = highgui.VideoCapture()
-    imshow = highgui.imshow(name="video",waitKey=2)
+    imshow = highgui.imshow(name="video", waitKey=2)
     sXY = SobelXY()
-    sobelshow = highgui.imshow(name="sobel",waitKey=-1)
+    sobelshow = highgui.imshow(name="sobel", waitKey= -1)
 
     plasm = ecto.Plasm()
     plasm.connect(video, "out", imshow, "input")
     plasm.connect(video, "out", sXY, "input")
     plasm.connect(sXY, "out", sobelshow, "input")
-
-    ecto.view_plasm(plasm)
+    
+    if debug:
+        ecto.view_plasm(plasm)
 
     while(imshow.outputs.out != 27):
         plasm.mark_dirty(video)
@@ -120,3 +116,4 @@ def video_test():
 
 if __name__ == '__main__':
     video_test()
+
