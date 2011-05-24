b2d18d7bc6d0bc46957593f545fd7a4cc55814e3
Ethan Rublee <ethan.rublee@gmail.com>
2011-05-23 23:46:10 -0700

    Clean up high gui.

--- modules/opencv/highgui.cpp
+++ modules/opencv/highgui.cpp
@@ -6,79 +6,78 @@
 
 #include <opencv2/highgui/highgui.hpp>
 
-//disable show in here
-#if 1
-#ifdef SHOW
-#undef SHOW
-#define SHOW() do{}while(false)
-#endif
-#endif
-
-struct ImageListReader :  ecto::module_interface
-{
-
-};
-
 using ecto::tendrils;
 
-struct VideoCapture :  ecto::module_interface
+struct VideoCapture: ecto::module_interface
 {
   void configure(const tendrils& params, tendrils& inputs, tendrils& outputs)
   {
-    SHOW();
-    int video_device = params.get<int> ("video_device");
-    std::string video_file = params.get<std::string> ("video_file");
+    video_device = params.get<int> ("video_device");
+    video_file = params.get<std::string> ("video_file");
     capture = cv::VideoCapture();
-    if (!video_file.empty())
-    {
-      capture.open(video_file);
-      if (!capture.isOpened())
-        throw std::runtime_error("Could not open the video file : " + video_file);
-    }
-    else
-    {
-      capture.open(video_device);
-      if (!capture.isOpened())
-        throw std::runtime_error("Could not open video device : " + video_device);
-    }
 
     //set outputs
     outputs.declare<cv::Mat> ("out", "A video frame.", cv::Mat());
     outputs.declare<int> ("frame_number", "The number of frames captured.", 0);
   }
 
-  void initialize(tendrils& params)
+  void open_video_device()
   {
-    SHOW();
-    params.declare<int> ("video_device", "The device ID to open.", 0);
-    params.declare<std::string> ("video_file", "A video file to read, leave empty to open a video device.", "");
+    if (capture.isOpened())
+      return;
+
+    if (!video_file.empty())
+    {
+      capture.open(video_file);
+      if (!capture.isOpened())
+        throw std::runtime_error(
+            "Could not open the video file : " + video_file);
+    }
+    else
+    {
+      capture.open(video_device);
+      if (!capture.isOpened())
+        throw std::runtime_error(
+            "Could not open video device : " + video_device);
+    }
   }
 
-  void process(const tendrils& params, const tendrils& inputs, tendrils& outputs)
+  void initialize(tendrils& params)
   {
-    SHOW();
+    params.declare<int> ("video_device", "The device ID to open.", 0);
+    params.declare<std::string> ("video_file",
+        "A video file to read, leave empty to open a video device.", "");
+  }
+
+  void process(const tendrils& params, const tendrils& inputs,
+      tendrils& outputs)
+  {
+    open_video_device();
+
     //outputs.get is a reference;
     capture >> outputs.get<cv::Mat> ("out");
+
     //increment our frame number.
     ++(outputs.get<int> ("frame_number"));
   }
   cv::VideoCapture capture;
+  int video_device;
+  std::string video_file;
 
 };
 
-struct imshow :  ecto::module_interface
+struct imshow: ecto::module_interface
 {
   void initialize(tendrils& params)
   {
-    SHOW();
     params.declare<std::string> ("name", "The window name", "image");
-    params.declare<int> ("waitKey", "Number of millis to wait, -1 for not at all, 0 for infinity.", -1);
+    params.declare<int> ("waitKey",
+        "Number of millis to wait, -1 for not at all, 0 for infinity.", -1);
     params.declare<bool> ("autoSize", "Autosize the window.", true);
   }
 
   void configure(const tendrils& params, tendrils& inputs, tendrils& outputs)
   {
-    SHOW();
     window_name_ = params.get<std::string> ("name");
     waitkey_ = params.get<int> ("waitKey");
     auto_size_ = params.get<bool> ("autoSize");
@@ -86,9 +85,9 @@ struct imshow :  ecto::module_interface
     outputs.declare<int> ("out", "Character pressed.");
   }
 
-  void process(const tendrils& params, const tendrils& inputs, tendrils& outputs)
+  void process(const tendrils& params, const tendrils& inputs,
+      tendrils& outputs)
   {
-    SHOW();
     const cv::Mat& image = inputs.get<cv::Mat> ("input");
     if (image.empty())
     {
@@ -118,6 +117,7 @@ struct imshow :  ecto::module_interface
 
 BOOST_PYTHON_MODULE(highgui)
 {
-  ecto::wrap<VideoCapture>("VideoCapture");
-  ecto::wrap<imshow>("imshow");
+  ecto::wrap<VideoCapture>("VideoCapture",
+      "Use to capture video from a camera or video file.");
+  ecto::wrap<imshow>("imshow", "Shows an image in a named window.");
 }
