228cb0c7910c0c8592d2c1508b1bce6348cf0de6
Ethan Rublee <ethan.rublee@gmail.com>
2011-07-26 23:52:26 -0700

    Adding depth to 3d module to calib.

---
+++ modules/opencv/calib/DepthTo3d.cpp
@@ -0,0 +1,90 @@
+#include <ecto/ecto.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+
+#include <iostream>
+using ecto::tendrils;
+namespace calib
+{
+  struct DepthTo3d
+  {
+    /**
+     *
+     * @param K
+     * @param depth a subregion of a matrix.
+     * @param points3d
+     * @param roi the roi that create the subregion.
+     */
+    static void
+    depthTo3d(const cv::Mat& K, const cv::Mat& depth, cv::Mat& points3d)
+    {
+      //allocate points.
+      cv::Mat_<float> points = cv::Mat_<float>(depth.size().area(), 3);
+      cv::Mat_<float>::iterator sp_begin = points.begin();
+      //grab camera params
+      float fx = K.at<float>(0, 0);
+      float fy = K.at<float>(1, 1);
+      float cx = K.at<float>(0, 2);
+      float cy = K.at<float>(1, 2);
+      std::cout << "K=" << K << std::endl;
+      // std::cout << fx << " "<< fy << std::endl;
+      // Create 3D points in one go.
+      cv::Size depth_size = depth.size();
+      for (int v = 0; v < depth_size.height; v++)
+      {
+        for (int u = 0; u < depth_size.width; u++)
+        {
+          float z = depth.at<uint16_t>(v, u) * 1/1000.0f;
+          *(sp_begin++) = (u - cx) * z / fx;
+          *(sp_begin++) = (v - cy) * z / fy;
+          *(sp_begin++) = z;
+        }
+      }
+      points3d = points; //return Nx3 matrix
+     // std::cout << points3d << std::endl;
+    }
+
+    typedef std::vector<cv::Point2f> points_t;
+    static void
+    declare_params(tendrils& p)
+    {
+    }
+
+    static void
+    declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
+    {
+      inputs.declare<cv::Mat>("K", "The calibration matrix").required(true);
+      inputs.declare<cv::Mat>("depth", "The depth image").required(true);
+      outputs.declare<cv::Mat>("points3d", "The 3d points.");
+
+    }
+
+    void
+    configure(tendrils& params, tendrils& inputs, tendrils& outputs)
+    {
+    }
+
+    /** Get the 2d keypoints and figure out their 3D position from the depth map
+     * @param inputs
+     * @param outputs
+     * @return
+     */
+    int
+    process(tendrils& inputs, tendrils& outputs)
+    {
+      cv::Mat K, depth;
+      inputs["K"] >> K;
+      inputs["depth"] >> depth;
+      K.clone().convertTo(K, CV_32F);
+      cv::Mat points3d;
+      depthTo3d(K, depth, points3d);
+      outputs["points3d"] << points3d;
+      return 0;
+    }
+  };
+
+}
+using namespace calib;
+ECTO_CELL(calib, DepthTo3d, "DepthTo3d", "Converts depth to 3d points.")
--- modules/projector/fitter.cpp
+++ modules/projector/fitter.cpp
@@ -33,8 +33,6 @@
  *
  */
 
-#include <boost/foreach.hpp>
-
 #include <ecto/ecto.hpp>
 
 #include <opencv2/calib3d/calib3d.hpp>
@@ -48,74 +46,75 @@
 
 namespace projector
 {
-void
-depth23d(const cv::Mat& K, const cv::Mat& depth, cv::Mat& points3d, const cv::Rect& roi)
-{
-  cv::Mat_<float> scaled_points = cv::Mat_<float>(depth.size().area(),3);
-  // Create the scaled keypoints
-  cv::Size depth_size = depth.size();
-  cv::Mat_<float>::const_iterator begin = depth.begin<float> (), end = depth.end<float> ();
-  cv::Mat_<float>::iterator sp_begin = scaled_points.begin();
-  cv::Point2f point = roi.tl();
-  while (begin != end)
+  void
+  depth23d(const cv::Mat& K, const cv::Mat& depth, cv::Mat& points3d, const cv::Rect& roi)
   {
-    float d = *(begin++);
-    *(sp_begin++) = point.x * d;
-    *(sp_begin++) = point.y * d;
-    *(sp_begin++) = d;
-    if (point.x < roi.br().x)
+    cv::Mat_<float> scaled_points = cv::Mat_<float>(depth.size().area(), 3);
+    // Create the scaled keypoints
+    cv::Size depth_size = depth.size();
+    cv::Mat_<float>::const_iterator begin = depth.begin<float>(), end = depth.end<float>();
+    cv::Mat_<float>::iterator sp_begin = scaled_points.begin();
+    cv::Point2f point = roi.tl();
+    while (begin != end)
     {
-      point.x += 1;
+      float d = *(begin++);
+      *(sp_begin++) = point.x * d;
+      *(sp_begin++) = point.y * d;
+      *(sp_begin++) = d;
+      if (point.x < roi.br().x)
+      {
+        point.x += 1;
+      }
+      else
+      {
+        point.y += 1;
+        point.x = roi.tl().x;
+      }
     }
-    else
+    // Figure out the 3D points
+    cv::Mat_<float> final_points_tmp;
+    cv::solve(K, scaled_points.t(), final_points_tmp);
+    points3d = final_points_tmp;
+  }
+  /**
+   *
+   * @param K
+   * @param depth a subregion of a matrix.
+   * @param points3d
+   * @param roi the roi that create the subregion.
+   */
+  void
+  depthTo3d(const cv::Mat& K, const cv::Mat& depth, cv::Mat& points3d, const cv::Rect& roi)
+  {
+    //allocate points.
+    cv::Mat_<float> points = cv::Mat_<float>(depth.size().area(), 3);
+    cv::Mat_<float>::iterator sp_begin = points.begin();
+    //grab camera params
+    float fx = K.at<float>(0, 0);
+    float fy = K.at<float>(1, 1);
+    float cx = K.at<float>(0, 2);
+    float cy = K.at<float>(1, 2);
+    // std::cout << fx << " "<< fy << std::endl;
+    // Create 3D points in one go.
+    cv::Size depth_size = depth.size();
+    for (int v = 0; v < depth_size.height; v++)
     {
-      point.y += 1;
-      point.x = roi.tl().x;
+      for (int u = 0; u < depth_size.width; u++)
+      {
+        cv::Point p(u, v);
+        cv::Point rp = p + roi.tl();
+        float z = depth.at<float>(p.y, p.x);
+        *(sp_begin++) = (rp.x - cx) * z / fx;
+        *(sp_begin++) = (rp.y - cy) * z / fy;
+        *(sp_begin++) = z;
+      }
     }
+    points3d = points.t(); //return 3xN matrix
   }
-  // Figure out the 3D points
-  cv::Mat_<float> final_points_tmp;
-  cv::solve(K, scaled_points.t(), final_points_tmp);
-  points3d = final_points_tmp;
-}
-/**
- *
- * @param K
- * @param depth a subregion of a matrix.
- * @param points3d
- * @param roi the roi that create the subregion.
- */
-void
-depthTo3d(const cv::Mat& K, const cv::Mat& depth, cv::Mat& points3d, const cv::Rect& roi)
-{
-  //allocate points.
-  cv::Mat_<float> points = cv::Mat_<float>(depth.size().area(),3);
-  cv::Mat_<float>::iterator sp_begin = points.begin();
-  //grab camera params
-  float fx = K.at<float>(0,0);
-  float fy = K.at<float>(1,1);
-  float cx = K.at<float>(0,2);
-  float cy = K.at<float>(1,2);
-  // std::cout << fx << " "<< fy << std::endl;
-  // Create 3D points in one go.
-  cv::Size depth_size = depth.size();
-  for(int v = 0; v < depth_size.height; v++)
+  void
+  solvePlane(cv::Mat xyz, cv::Mat& plane)
   {
-  for(int u = 0; u < depth_size.width; u++)
-  {
-      cv::Point p(u,v);
-      cv::Point rp = p + roi.tl();
-      float z = depth.at<float>(p.y,p.x);
-      *(sp_begin++) = ( rp.x - cx) * z/fx ;
-      *(sp_begin++) = ( rp.y - cy) * z/fy;
-      *(sp_begin++) = z;
-    }
-  }
-  points3d = points.t();//return 3xN matrix
-}
-void solvePlane(cv::Mat xyz,cv::Mat& plane)
-{
-  //points is 3,N
+    //points is 3,N
 //    % Set up constraint equations of the form  AB = 0,
 //    % where B is a column vector of the plane coefficients
 //    % in the form   b(1)*X + b(2)*Y +b(3)*Z + b(4) = 0.
@@ -128,40 +127,41 @@ void solvePlane(cv::Mat xyz,cv::Mat& plane)
 //
 //    [u d v] = svd(A);        % Singular value decomposition.
 //    B = v(:,4);              % Solution is last column of v.
-  cv::Mat A = xyz;
-  A.resize(4,cv::Scalar(1));
-  cv::SVD svd(A.t());
-  plane = svd.vt.row(svd.vt.rows-1);
-}
+    cv::Mat A = xyz;
+    A.resize(4, cv::Scalar(1));
+    cv::SVD svd(A.t());
+    plane = svd.vt.row(svd.vt.rows - 1);
+  }
 
-template<typename T>
-int sign(T f) {
-    if (f>0)
-        return 1;
+  template<typename T>
+  int
+  sign(T f)
+  {
+    if (f > 0)
+      return 1;
     else
-        return -1;
-}
-
+      return -1;
+  }
 
   void
   solveRT(cv::Mat_<float> plane, cv::Mat_<float>& R, cv::Mat_<float>& T)
   {
-    float /*a = plane(0), b = plane(1),*/ c = plane(2), d = plane(3);
+    float /*a = plane(0), b = plane(1),*/c = plane(2), d = plane(3);
     float z = -d / c;
-    cv::Mat_<float> normal = plane.colRange(0,3).t();
+    cv::Mat_<float> normal = plane.colRange(0, 3).t();
     normal = normal / cv::norm(normal);
     T = (cv::Mat_<float>(3, 1) << 0, 0, z);
     //construct an ortho normal basis.
     cv::Mat_<float> X = (cv::Mat_<float>(3, 1) << 1, 0, 0); //unit x vector.
     cv::Mat_<float> Vx = X - X.dot(normal) * normal;
     cv::Mat_<float> Y = (cv::Mat_<float>(3, 1) << 0, 1, 0);
-    cv::Mat_<float> Vy = (cv::Mat_<float>(3, 1) << 0, -1, 0);//make y go up.
+    cv::Mat_<float> Vy = (cv::Mat_<float>(3, 1) << 0, -1, 0); //make y go up.
     Vy = Vy - Vy.dot(normal) * normal;
     Vx = Vx / cv::norm(Vx);
     Vy = Vy / cv::norm(Vy);
 
     cv::Mat_<float> Z = (cv::Mat_<float>(3, 1) << 0, 0, 1);
-    cv::Mat_<float> Vz = Vx.cross(Vy);//construct a right handed basis. Z = X cross Y
+    cv::Mat_<float> Vz = Vx.cross(Vy); //construct a right handed basis. Z = X cross Y
 
     //Build up correlation matrix
     cv::Mat_<float> C = cv::Mat_<float>::zeros(3, 3);
@@ -182,57 +182,61 @@ int sign(T f) {
     //according to the paper TM = U * diag(1,1,s) * V^T
     R = svd.u * diagm * svd.vt;
   }
-using ecto::tendrils;
+  using ecto::tendrils;
 
-struct PlaneFitter
-{
-  typedef std::vector<cv::Point2f> points_t;
-  static void declare_params(tendrils& p)
+  struct PlaneFitter
   {
-  }
+    typedef std::vector<cv::Point2f> points_t;
+    static void
+    declare_params(tendrils& p)
+    {
+    }
 
-  static void declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
-  {
-    inputs.declare<cv::Mat>("K", "The calibration matrix");
-    inputs.declare<cv::Mat>("depth", "The depth image");
-    outputs.declare<cv::Mat>("R", "The output R vec");
-    outputs.declare<cv::Mat>("T", "The output T vec");
-    outputs.declare<bool>("found", "Found a plane",true);
+    static void
+    declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
+    {
+      inputs.declare<cv::Mat>("K", "The calibration matrix");
+      inputs.declare<cv::Mat>("depth", "The depth image");
+      outputs.declare<cv::Mat>("R", "The output R vec");
+      outputs.declare<cv::Mat>("T", "The output T vec");
+      outputs.declare<bool>("found", "Found a plane", true);
 
-  }
+    }
 
-  void configure(tendrils& params, tendrils& inputs, tendrils& outputs)
-  {
-  }
+    void
+    configure(tendrils& params, tendrils& inputs, tendrils& outputs)
+    {
+    }
 
-  /** Get the 2d keypoints and figure out their 3D position from the depth map
-   * @param inputs
-   * @param outputs
-   * @return
-   */
-  int process(tendrils& inputs, tendrils& outputs)
-  {
-    cv::Mat K,depth;
-    inputs["K"] >> K;
-    inputs["depth"] >> depth;
-    K.clone().convertTo(K, CV_32F);
-    cv::Mat points3d;
-    int roi_size = 40;//sets the sample region MxM
-    cv::Rect roi(depth.size().width/2 - roi_size/2,depth.size().height/2 - roi_size/2,roi_size,roi_size);
-    cv::Mat depth_sub = depth(roi); //grab the sample region
-    depthTo3d(K,depth_sub,points3d,roi);
-    cv::Mat plane;
-    //std::cout <<"points: " << points3d.t() << std::endl;
-    solvePlane(points3d,plane);
-    //std::cout << "Plane = " << plane << std::endl;
-    cv::Mat_<float> R, T;
-    solveRT(plane,R,T);
-    //std::cout << "R = " << R << std::endl;
-    //std::cout << "T = " << T << std::endl;
-    outputs["R"] << cv::Mat(R);
-    outputs["T"] << cv::Mat(T);
-    return 0;
-  }
-};
+    /** Get the 2d keypoints and figure out their 3D position from the depth map
+     * @param inputs
+     * @param outputs
+     * @return
+     */
+    int
+    process(tendrils& inputs, tendrils& outputs)
+    {
+      cv::Mat K, depth;
+      inputs["K"] >> K;
+      inputs["depth"] >> depth;
+      K.clone().convertTo(K, CV_32F);
+      cv::Mat points3d;
+      int roi_size = 40; //sets the sample region MxM
+      cv::Rect roi(depth.size().width / 2 - roi_size / 2, depth.size().height / 2 - roi_size / 2, roi_size, roi_size);
+      cv::Mat depth_sub = depth(roi); //grab the sample region
+      depthTo3d(K, depth_sub, points3d, roi);
+      cv::Mat plane;
+      //std::cout <<"points: " << points3d.t() << std::endl;
+      solvePlane(points3d, plane);
+      //std::cout << "Plane = " << plane << std::endl;
+      cv::Mat_<float> R, T;
+      solveRT(plane, R, T);
+      //std::cout << "R = " << R << std::endl;
+      //std::cout << "T = " << T << std::endl;
+      outputs["R"] << cv::Mat(R);
+      outputs["T"] << cv::Mat(T);
+      return 0;
+    }
+  };
 }
 ECTO_CELL(projector, projector::PlaneFitter, "PlaneFitter", "Finds the plane.");
