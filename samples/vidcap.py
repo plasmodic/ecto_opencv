99680fc17f035d22d66f5f9c716ae81d3cf30391
Ethan Rublee <ethan.rublee@gmail.com>
2011-08-24 21:48:58 -0700

    using new options.

--- samples/vidcap.py
+++ samples/vidcap.py
@@ -2,24 +2,22 @@
 #
 # Simple vid cap
 #
-import ecto
+import ecto, ecto.opts
 from ecto_opencv import highgui, calib, imgproc
 
-plasm = ecto.Plasm()
-video_cap = highgui.VideoCapture(video_device=0)
+video_cap = highgui.VideoCapture(video_device=0, width=640, height=480)
 
 fps = highgui.FPSDrawer()
 
 video_display = highgui.imshow('imshow',
                                name='video_cap', waitKey=2)
 
+plasm = ecto.Plasm()
 plasm.connect(video_cap['image'] >> fps['image'],
               fps['image'] >> video_display['input'],
               )
 
-
 if __name__ == '__main__':
-    ecto.view_plasm(plasm)
-    sched = ecto.schedulers.Threadpool(plasm)
-    sched.execute()
-
+    ecto.opts.parse_args_run_plasm(plasm,
+                                   description='Capture a video from the device and display it.'
+                                   )
