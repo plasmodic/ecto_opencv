3fb7b6875f70f46c5ea33c370b413ea4d4229cfe
Ethan Rublee <ethan.rublee@gmail.com>
2011-08-25 00:35:45 -0700

    Adding desktop tests, and making all samples conform to uniform argparse stuffs.

--- samples/vidcap.py
+++ samples/vidcap.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
-#
-# Simple vid cap
-#
-import ecto, ecto.opts
-from ecto_opencv import highgui, calib, imgproc
+import ecto
+from ecto_opencv.highgui import VideoCapture, imshow, FPSDrawer
 
-video_cap = highgui.VideoCapture(video_device=0, width=640, height=480)
-
-fps = highgui.FPSDrawer()
-
-video_display = highgui.imshow('imshow',
-                               name='video_cap', waitKey=2)
+video_cap = VideoCapture(video_device=0, width=640, height=480)
+fps = FPSDrawer()
+video_display = imshow(name='video_cap', waitKey=2)
 
 plasm = ecto.Plasm()
 plasm.connect(video_cap['image'] >> fps['image'],
@@ -18,6 +12,5 @@ plasm.connect(video_cap['image'] >> fps['image'],
               )
 
 if __name__ == '__main__':
-    ecto.opts.parse_args_run_plasm(plasm,
-                                   description='Capture a video from the device and display it.'
-                                   )
+    from ecto.opts import doit
+    doit(plasm, description='Capture a video from the device and display it.')
