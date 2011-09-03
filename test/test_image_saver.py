d2219235a811d541e0169c2d7aec182a133a88dd
Ethan Rublee <ethan.rublee@gmail.com>
2011-09-03 12:19:23 -0700

    Adding testing cells and making the warning go away in the tests on build bot.

--- test/test_image_saver.py
+++ test/test_image_saver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import ecto
 import ecto_opencv
-from ecto_opencv import highgui
+from ecto_opencv import highgui, opencv_test
+import os.path
 
 try:
     saver = highgui.ImageSaver(filename_format='img.png')
@@ -8,6 +10,14 @@ except RuntimeError, e:
     assert 'boost::too_many_args:' in str(e)
 
 saver = highgui.ImageSaver(filename_format='img_%05d.png')
-saver.configure()
-saver.process()
 
+plasm = ecto.Plasm()
+plasm.connect(opencv_test.ImageGen()['image'] >> saver['image'])
+sched = ecto.schedulers.Singlethreaded(plasm)
+sched.execute(niter=1)
+
+assert os.path.isfile('img_00000.png')
+stats =  os.stat('img_00000.png')
+print stats
+assert stats.st_size > 1000
+os.remove('img_00000.png')
