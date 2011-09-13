140a541780a27e06b6fcff66cac1c125d4baa532
Vincent Rabaud <vincent.rabaud@gmail.com>
2011-09-12 20:41:29 -0700

    - implement the depth to 3d in separate files for reuse

--- modules/opencv/calib/DepthTo3d.cpp
+++ modules/opencv/calib/DepthTo3d.cpp
@@ -42,70 +42,15 @@
 #include <opencv2/features2d/features2d.hpp>
 
 #include <iostream>
+
+#include "impl/depth_to_3d.h"
+
 using ecto::tendrils;
 namespace calib
 {
   struct DepthTo3dSparse
   {
-    /**
-     * @param K
-     * @param uv the X,Y coordinates of the points in the image (n_points by 2 matrix)
-     * @param depth the full depth image
-     * @param points3d the resulting 3d points
-     */
-    static void
-    depthTo3d_sparse(const cv::Mat& K, const cv::Mat& in_uv, const cv::Mat& depth, cv::Mat& points3d)
-    {
-      // Divide by two as we have 2 channels
-      unsigned int n_points = in_uv.total() / 2;
-
-      //grab camera params
-      float fx = K.at<float>(0, 0);
-      float fy = K.at<float>(1, 1);
-      float cx = K.at<float>(0, 2);
-      float cy = K.at<float>(1, 2);
-      cv::Mat uv;
-      in_uv.convertTo(uv, CV_32FC2);
-      uv.reshape(1, n_points);
-
-      if (uv.empty())
-      {
-        std::cerr << "Warning uv is empty!" << std::endl;
-        return;
-      }
-      // Get the depth
-      cv::Mat_<float> zs = cv::Mat_<float>(n_points, 1);
-      if (depth.depth() == CV_16U)
-      {
-        for (unsigned int i = 0; i < n_points; ++i)
-        {
-          uint16_t depth_i = depth.at<uint16_t>(uv.at<float>(i, 1), uv.at<float>(i, 0));
-          if ((depth_i == 0) || (depth_i == std::numeric_limits<uint16_t>::max()))
-            zs(i, 0) = std::numeric_limits<float>::quiet_NaN();
-          else
-            zs(i, 0) = depth_i / 1000.0f;
-        }
-      }
-      else if (depth.depth() == CV_32F)
-      {
-        for (unsigned int i = 0; i < n_points; ++i)
-          zs(i, 0) = depth.at<float>(uv.at<float>(i, 1), uv.at<float>(i, 0));
-      }
-
-      // Store into the final points
-      cv::Mat_<float> coordinates[3] =
-      { (uv.col(0) - cx).mul(zs) / fx, (uv.col(1) - cy).mul(zs) / fy, zs };
-      cv::Mat tmp_points;
-      cv::merge(coordinates, 3, tmp_points);
-      points3d = tmp_points.reshape(3, 1);
-    }
-
     typedef std::vector<cv::Point2f> points_t;
-    static void
-    declare_params(tendrils& p)
-    {
-    }
-
     static void
     declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
     {
@@ -113,11 +58,6 @@ namespace calib
       inputs.declare<cv::Mat>("points", "The 2d coordinates (matrix with 2 channels)").required(true);
       inputs.declare<cv::Mat>("depth", "The depth image").required(true);
       outputs.declare<cv::Mat>("points3d", "The 3d points, 1 by n_points with 3 channels (x, y and z).");
-    }
-
-    void
-    configure(const tendrils& params, const tendrils& inputs, const tendrils& outputs)
-    {
     }
 
     /** Get the 2d keypoints and figure out their 3D position from the depth map
@@ -132,11 +72,14 @@ namespace calib
       inputs["K"] >> K;
       const cv::Mat &depth = inputs.get<cv::Mat>("depth"), &uv = inputs.get<cv::Mat>("points");
 
-      K.clone().convertTo(K, CV_32F);
+      cv::Mat uv_reshaped = uv.reshape(1, uv.rows);
       cv::Mat points3d;
-      depthTo3d_sparse(K, uv, depth, points3d);
+      cv::Mat u = uv_reshaped.col(0), v = uv_reshaped.col(1);
+      depthTo3dSparse(K, depth, u, v, points3d);
+
       outputs["points3d"] << points3d;
-      return 0;
+
+      return ecto::OK;
     }
   };
 
@@ -154,99 +97,10 @@ namespace calib
     depthTo3d(const cv::Mat& K, const cv::Mat& depth, const cv::Mat& mask, cv::Mat& points3d)
     {
       // Create 3D points in one go.
-      cv::Size depth_size = depth.size();
       if (!mask.empty())
-      {
-        cv::Mat_<float> uv = cv::Mat_<float>(depth.size().area(), 2);
-        cv::Mat_<uchar> uchar_mask = mask;
-        if (mask.depth() != (CV_8U))
-          mask.convertTo(uchar_mask, CV_8U);
-
-        // Figure out the interesting indices
-        unsigned int n_points = 0;
-        const uchar* r;
-        for (int v = 0; v < depth_size.height; v++)
-        {
-          r = uchar_mask.ptr<uchar>(v, 0);
-          for (int u = 0; u < depth_size.width; u++, ++r)
-          {
-            if (*r)
-            {
-              uv(n_points, 0) = u;
-              uv(n_points, 1) = v;
-              ++n_points;
-            }
-          }
-        }
-
-        // Get the depth
-        uv.reserve(n_points);
-
-        DepthTo3dSparse::depthTo3d_sparse(K, uv, depth, points3d);
-      }
+        depthTo3dMask(K, depth, mask, points3d);
       else
-      {
-        //allocate points.
-        cv::Mat points = cv::Mat(depth.rows, depth.cols, CV_32FC3);
-        float * sp_begin = reinterpret_cast<float *>(points.data);
-        //grab camera params
-        float fx = K.at<float>(0, 0);
-        float fy = K.at<float>(1, 1);
-        float cx = K.at<float>(0, 2);
-        float cy = K.at<float>(1, 2);
-
-        if (depth.depth() == CV_16U)
-        {
-          const uint16_t* r;
-          for (int v = 0; v < depth_size.height; v++)
-          {
-            r = depth.ptr<uint16_t>(v, 0);
-            for (int u = 0; u < depth_size.width; u++)
-            {
-              uint16_t fpz = *(r++);
-              if ((fpz == 0) || (fpz == std::numeric_limits<uint16_t>::max()))
-              {
-                *(sp_begin++) = std::numeric_limits<float>::quiet_NaN();
-                *(sp_begin++) = std::numeric_limits<float>::quiet_NaN();
-                *(sp_begin++) = std::numeric_limits<float>::quiet_NaN();
-              }
-              else
-              {
-                float z = fpz / 1000.0f;
-                *(sp_begin++) = (u - cx) * z / fx;
-                *(sp_begin++) = (v - cy) * z / fy;
-                *(sp_begin++) = z;
-              }
-            }
-          }
-        }
-        else if (depth.depth() == CV_32F)
-        {
-          const float* r;
-          for (int v = 0; v < depth_size.height; v++)
-          {
-            r = depth.ptr<float>(v, 0);
-
-            for (int u = 0; u < depth_size.width; u++)
-            {
-              float z = *(r++);
-              if (std::isfinite(z))
-              {
-                *(sp_begin++) = (u - cx) * z / fx;
-                *(sp_begin++) = (v - cy) * z / fy;
-                *(sp_begin++) = z;
-              }
-              else
-              {
-                *(sp_begin++) = z;
-                *(sp_begin++) = z;
-                *(sp_begin++) = z;
-              }
-            }
-          }
-        }
-        points3d = points; //return Nx3 matrix
-      }
+        calib::depthTo3d(K, depth, points3d);
     }
 
     typedef std::vector<cv::Point2f> points_t;
@@ -273,7 +127,7 @@ namespace calib
       inputs["K"] >> K;
       inputs["depth"] >> depth;
       inputs["mask"] >> mask;
-      K.clone().convertTo(K, CV_32F);
+
       cv::Mat points3d;
       depthTo3d(K, depth, mask, points3d);
 
---
+++ modules/opencv/calib/impl/depth_to_3d.cpp
@@ -0,0 +1,263 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2009, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <iostream>
+
+#include "depth_to_3d.h"
+
+namespace calib
+{
+  /**
+   * @param K
+   * @param depth the depth image
+   * @param mask the mask of the points to consider (can be empty)
+   * @param points3d the resulting 3d points
+   */
+  void
+  depthTo3d_from_uvz(const cv::Mat& in_K, const cv::Mat& u_mat, const cv::Mat& v_mat, const cv::Mat& z_mat,
+                     cv::Mat& points3d)
+  {
+    //grab camera params
+    cv::Mat_<float> K;
+    in_K.convertTo(K, CV_32F);
+    float fx = K(0, 0);
+    float fy = K(1, 1);
+    float cx = K(0, 2);
+    float cy = K(1, 2);
+
+    cv::Mat_<float> coordinates[3] =
+    { (u_mat - cx).mul(z_mat) / fx, (v_mat - cy).mul(z_mat) / fy, z_mat };
+    cv::Mat tmp_points;
+    cv::merge(coordinates, 3, tmp_points);
+    points3d = tmp_points.reshape(3, 1);
+  }
+
+  /**
+   * @param K
+   * @param depth the depth image
+   * @param u_mat the list of x coordinates
+   * @param v_mat the list of matching y coordinates
+   * @param points3d the resulting 3d points
+   */
+  void
+  depthTo3dSparse(const cv::Mat& in_K, const cv::Mat& depth, const cv::Mat& u_mat, const cv::Mat& v_mat,
+                  cv::Mat& points3d)
+  {
+    // Make sure we use foat types
+    cv::Mat_<float> u_float, v_float;
+    if (u_mat.depth() == (CV_32F))
+      u_float = u_mat;
+    else
+      u_mat.convertTo(u_float, CV_32F);
+    if (v_mat.depth() == (CV_32F))
+      v_float = v_mat;
+    else
+      v_mat.convertTo(v_float, CV_32F);
+
+    unsigned int n_points = u_mat.rows;
+    // Fill the depth matrix
+    cv::Mat_<float> z_float = cv::Mat_<float>(n_points, 1);
+
+    const float * v_ptr = reinterpret_cast<float*>(v_float.data), *u_ptr = reinterpret_cast<float*>(u_float.data);
+    float *z_ptr = reinterpret_cast<float*>(z_float.data);
+    if (depth.depth() == CV_16U)
+    {
+      float z_min = 1, z_max = 0;
+      for (unsigned int i = 0; i < n_points; ++i, ++u_ptr, ++v_ptr, ++z_ptr)
+      {
+        uint16_t depth_i = depth.at<uint16_t>(*v_ptr, *u_ptr);
+        if ((depth_i == 0) || (depth_i == std::numeric_limits<uint16_t>::max()))
+          *z_ptr = std::numeric_limits<float>::quiet_NaN();
+        else
+        {
+          *z_ptr = depth_i / 1000.0f;
+          z_min = std::min(z_min, *z_ptr);
+          z_max = std::max(z_max, *z_ptr);
+        }
+      }
+      std::cout << z_min << " " << z_max << std::endl;
+    }
+    else if (depth.depth() == CV_32F)
+    {
+      for (unsigned int i = 0; i < n_points; ++i, ++u_ptr, ++v_ptr, ++z_ptr)
+        *z_ptr = depth.at<float>(*v_ptr, *u_ptr);
+    }
+
+    depthTo3d_from_uvz(in_K, u_float, v_float, z_float, points3d);
+  }
+
+  /**
+   * @param K
+   * @param depth the depth image
+   * @param mask the mask of the points to consider (can be empty)
+   * @param points3d the resulting 3d points
+   */
+  void
+  depthTo3dMask(const cv::Mat& K, const cv::Mat& depth, const cv::Mat& mask, cv::Mat& points3d)
+  {
+    // Create 3D points in one go.
+    cv::Size depth_size = depth.size();
+    cv::Mat_<float> u_mat = cv::Mat_<float>(depth_size.area(), 1), v_mat = cv::Mat_<float>(depth_size.area(), 1),
+        z_mat = cv::Mat_<float>(depth_size.area(), 1);
+
+    cv::Mat_<uchar> uchar_mask = mask;
+    if (mask.depth() != (CV_8U))
+      mask.convertTo(uchar_mask, CV_8U);
+
+    // Figure out the interesting indices
+    unsigned int n_points = 0;
+    const uchar* r;
+    if (depth.depth() == CV_16U)
+    {
+      // Raw data from the Kinect has int
+      for (int v = 0; v < depth_size.height; v++)
+      {
+        r = uchar_mask.ptr<uchar>(v, 0);
+        for (int u = 0; u < depth_size.width; u++, ++r)
+          if (*r)
+          {
+            u_mat(n_points, 0) = u;
+            v_mat(n_points, 0) = v;
+            uint16_t depth_i = depth.at<uint16_t>(v, u);
+            if ((depth_i == std::numeric_limits<uint16_t>::min()) || (depth_i == std::numeric_limits<uint16_t>::max()))
+              z_mat(n_points, 0) = std::numeric_limits<float>::quiet_NaN();
+            else
+              z_mat(n_points, 0) = depth_i / 1000.0f;
+            ++n_points;
+          }
+      }
+    }
+    else
+    {
+      // If the depth is already floats, no need to divide by 1000
+      for (int v = 0; v < depth_size.height; v++)
+      {
+        r = uchar_mask.ptr<uchar>(v, 0);
+        for (int u = 0; u < depth_size.width; u++, ++r)
+          if (*r)
+          {
+            u_mat(n_points, 0) = u;
+            v_mat(n_points, 0) = v;
+            float z = depth.at<float>(v, u);
+            if ((z != z) || (z == std::numeric_limits<float>::max()))
+              z_mat(n_points, 0) = std::numeric_limits<float>::quiet_NaN();
+            else
+              z_mat(n_points, 0) = z / 1000.0f;
+            ++n_points;
+          }
+      }
+    }
+    u_mat.resize(n_points);
+    v_mat.resize(n_points);
+    z_mat.resize(n_points);
+
+    depthTo3d_from_uvz(K, u_mat, v_mat, z_mat, points3d);
+  }
+
+  /**
+   * @param K
+   * @param depth the depth image
+   * @param points3d the resulting 3d points
+   */
+  void
+  depthTo3d(const cv::Mat& in_K, const cv::Mat& in_depth, cv::Mat& points3d)
+  {
+    // Create 3D points in one go.
+    cv::Size depth_size = in_depth.size();
+    cv::Mat_<float> u_mat = cv::Mat_<float>(depth_size), v_mat = cv::Mat_<float>(depth_size), z_mat = cv::Mat_<float>(
+        depth_size);
+
+    // If we compute on the whole matrix
+    cv::Mat_<float> K;
+    in_K.convertTo(K, CV_32F);
+
+    //grab camera params
+    float fx = K.at<float>(0, 0);
+    float fy = K.at<float>(1, 1);
+    float cx = K.at<float>(0, 2);
+    float cy = K.at<float>(1, 2);
+
+    // Build z
+    float * sp_begin = reinterpret_cast<float *>(z_mat.data);
+    if (in_depth.depth() == CV_16U)
+    {
+      const uint16_t* r;
+      for (int v = 0; v < depth_size.height; v++)
+      {
+        r = in_depth.ptr<uint16_t>(v, 0);
+        for (int u = 0; u < depth_size.width; u++)
+        {
+          uint16_t fpz = *(r++);
+          if ((fpz == std::numeric_limits<uint16_t>::min()) || (fpz == std::numeric_limits<uint16_t>::max()))
+            *(sp_begin++) = std::numeric_limits<float>::quiet_NaN();
+          else
+            *(sp_begin++) = fpz / 1000.0f;
+        }
+      }
+    }
+    else if (in_depth.depth() == CV_32F)
+    {
+      const float* r;
+      for (int v = 0; v < depth_size.height; v++)
+      {
+        r = in_depth.ptr<float>(v, 0);
+        for (int u = 0; u < depth_size.width; ++u, ++r)
+        {
+          float z = *r;
+          if ((z != z) || (z == std::numeric_limits<float>::max()))
+            *(sp_begin++) = z;
+          else
+            *(sp_begin++) = z;
+        }
+      }
+    }
+
+    // Build the set of u's and v's
+    cv::Mat_<float> us = cv::Mat_<float>(1, depth_size.width), vs = cv::Mat_<float>(depth_size.height, 1);
+    for (int v = 0; v < depth_size.height; v++)
+      vs(0, v) = (v - cy) / fy;
+    for (int u = 0; u < depth_size.width; u++)
+      us(0, u) = (u - cx) / fx;
+    cv::repeat(us, depth_size.height, 1, u_mat);
+    cv::repeat(vs, 1, depth_size.width, v_mat);
+
+    // Compute all the coordinates
+    cv::Mat_<float> coordinates[3] =
+    { u_mat.mul(z_mat), v_mat.mul(z_mat), z_mat };
+    cv::Mat tmp_points;
+    cv::merge(coordinates, 3, tmp_points);
+    points3d = tmp_points.reshape(3, in_depth.rows);
+  }
+}
