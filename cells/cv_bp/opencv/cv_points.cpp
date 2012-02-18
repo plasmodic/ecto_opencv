65cc4e3c889c411aebeaf33eb41e2448335d2657
Ethan Rublee <ethan.rublee@gmail.com>
2012-02-17 16:23:20 -0800

    more better wrapper.

--- cells/cv_bp/opencv/cv_points.cpp
+++ cells/cv_bp/opencv/cv_points.cpp
@@ -12,27 +12,55 @@ namespace
 {
 
   template<typename T>
-  cv::Point_<T>* make_point(bp::object o)
-  {
-    T x = bp::extract<T>(o[0]);
-    T y = bp::extract<T>(o[1]);
-    return  new cv::Point_<T>(x,y);
-  }
-
-  template<typename T>
-  void wrap_point(const std::string& name)
+  struct PointWrapper
   {
     typedef cv::Point_<T> Point_t;
-    bp::class_<Point_t> Point_(name.c_str());
-    Point_.def(bp::init<>());
-    Point_.def(bp::init<T, T>());
-    Point_.def(bp::init<Point_t>());
-    Point_.def("__init__",bp::make_constructor(&make_point<T>));
-    Point_.def_readwrite("x", &Point_t::x);
-    Point_.def_readwrite("y", &Point_t::y);
-    Point_.def_readwrite("dot", &Point_t::dot);
-    Point_.def_readwrite("inside", &Point_t::inside);
-  }
+    typedef std::vector<Point_t> VectorPoint_t;
+
+    static Point_t make_point(bp::object o)
+    {
+      T x = bp::extract<T>(o[0]);
+      T y = bp::extract<T>(o[1]);
+      return  Point_t(x,y);
+    }
+
+    static boost::shared_ptr<Point_t> make_point_ptr(bp::object o)
+    {
+      return boost::shared_ptr<Point_t>(new Point_t(make_point(o)));
+    }
+
+    static boost::shared_ptr<VectorPoint_t> make_points(bp::object o)
+    {
+      size_t l = bp::len(o);
+      boost::shared_ptr<VectorPoint_t> v(new VectorPoint_t(l));
+      for(size_t i = 0; i < l; i++){
+        (*v)[i] = make_point(o[i]);
+      }
+      return v;
+    }
+
+    static void wrap_point(const std::string& name)
+    {
+      typedef cv::Point_<T> Point_t;
+      bp::class_<Point_t,boost::shared_ptr<Point_t> > Point_(name.c_str());
+      Point_.def(bp::init<>());
+      Point_.def(bp::init<T, T>());
+      Point_.def(bp::init<Point_t>());
+      Point_.def("__init__",bp::make_constructor(&make_point_ptr));
+      Point_.def_readwrite("x", &Point_t::x);
+      Point_.def_readwrite("y", &Point_t::y);
+      Point_.def_readwrite("dot", &Point_t::dot);
+      Point_.def_readwrite("inside", &Point_t::inside);
+
+      //bp::implicitly_convertible<bp::object,Point_t>();
+      std::string vec_name = "Vector"+name;
+      bp::class_<VectorPoint_t, boost::shared_ptr<VectorPoint_t> > (vec_name.c_str())
+        .def(bp::vector_indexing_suite<VectorPoint_t>())
+        .def("__init__", bp::make_constructor(&make_points));
+
+      //      bp::implicitly_convertible<bp::object,VectorPoint_t>();
+    }
+  };
 
 template<typename T>
   void wrap_rect(const std::string& name)
@@ -67,16 +95,13 @@ namespace opencv_wrappers
     Size_.def_readwrite("height", &cv::Size::height);
     Size_.def("area", &cv::Size::area);
 
-    wrap_point<int> ("Point");
-    wrap_point<float> ("Point2f");
-    wrap_point<double> ("Point2d");
+    PointWrapper<int>::wrap_point("Point");
+    PointWrapper<float>::wrap_point("Point2f");
+    PointWrapper<double>::wrap_point("Point2d");
 
     wrap_rect<int> ("Rect");
     wrap_rect<float> ("Rectf");
     wrap_rect<double> ("Rectd");
-
-    bp::class_<std::vector<cv::Point2f> > ("VectorPoint2f")
-     .def(bp::vector_indexing_suite<std::vector<cv::Point2f> >());
 
   }
 }
