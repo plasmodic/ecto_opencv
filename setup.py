33417554c30fcd8a68a7fba9870e5ad5d83cca3a
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-27 00:32:13 -0700

    rename the python folder

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(name='Ecto OpenCV',
       version='1.0.0',
       description='Ecto bindings for OpenCV',
-      packages=['ecto_opencv_py'],
+      packages=['ecto_opencv'],
       package_dir={'':'python'}
 )
