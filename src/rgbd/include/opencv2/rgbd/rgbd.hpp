cf3215bd27c90a562af19410e59a989ab6bc2e5d
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-22 19:51:12 -0700

    add a function to check if a depth is valid

--- src/rgbd/include/opencv2/rgbd/rgbd.hpp
+++ src/rgbd/include/opencv2/rgbd/rgbd.hpp
@@ -38,10 +38,26 @@
 
 #ifdef __cplusplus
 
+#include <limits.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/core/types_c.h>
 
 namespace cv
 {
+  /** Checks if the value is a valid depth. For CV_16U or CV_16S, the convention is to be invalid if it is
+   * a limit. For a float, we just check if it is a NaN
+   * @param depth
+   * @param in_K
+   * @param in_points
+   * @param points3d
+   */
+  template<typename T>
+  bool
+  isValidDepth(const T & depth)
+  {
+    return (depth != std::numeric_limits < T > ::min()) && (depth != std::numeric_limits < T > ::max());
+  }
+
   /**
    * @param depth the depth image
    * @param K
--- src/rgbd/src/utils.cpp
+++ src/rgbd/src/utils.cpp
@@ -38,6 +38,17 @@
 
 namespace cv
 {
+  /** Specialization for floats
+   * @param depth
+   * @return
+   */
+  template<>
+  bool
+  isValidDepth(const float & depth)
+  {
+    return cvIsNaN(depth);
+  }
+
   /** If the input image is of type CV_16UC1 (like the Kinect one), the image is converted to floats, divided
    * by 1000 to get a depth in meters, and the values 0 are converted to std::numeric_limits<float>::quiet_NaN()
    * Otherwise, the image is simply converted to floats
