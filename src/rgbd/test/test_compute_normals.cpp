6a1ab7bd62c430de1e9f8d8138ca65b6c44dc407
Vincent Rabaud <vrabaud@willowgarage.com>
2012-05-30 09:54:56 -0500

    add a test for DepthTo3d

--- src/rgbd/test/test_compute_normals.cpp
+++ src/rgbd/test/test_compute_normals.cpp
@@ -17,28 +17,32 @@ protected:
     {
       // K from a VGA Kinect
       cv::Mat K = (cv::Mat_<float>(3, 3) << 525., 0., 319.5, 0., 525., 239.5, 0., 0., 1.);
+      K = (cv::Mat_<float>(3, 3) << 1., 0., 320, 0., 1,240, 0., 0., 1.);
 
       // Create a random plane
       cv::RNG rng;
       cv::Mat_<float> plane(1, 4);
       rng.fill(plane, cv::RNG::UNIFORM, -10, 10);
+      plane(0, 0) = 0;
+      plane(0, 1) = 0;
+      plane(0, 2) = 1;
+      plane(0, 3) = -1;
 
       // Create some 3d points on the plane
       int rows = 480, cols = 640;
       cv::Mat_<float> depth(rows, cols);
       {
         cv::Mat_<float> depth_image = cv::Mat_<float>::ones(rows, cols);
-        cv::Mat_<cv::Vec3f> points3d_ini;
-        cv::depthTo3d(depth_image, K, points3d_ini);
+        cv::Mat K_inv = K.inv();
         // Fix the scale of each point to fall on the plane
         for (int y = 0; y < rows; ++y)
           for (int x = 0; x < cols; ++x)
           {
-            cv::Vec3f &point = points3d_ini(y, x);
+            cv::Mat_<float> point = K_inv * (cv::Mat_<float>(3, 1) << x, y, 1);
             point = point
                     * (-plane(0, 3)
-                       / (point.val[0] * plane(0, 0) + point.val[1] * plane(0, 1) + point.val[2] * plane(0, 2)));
-            depth(y, x) = point.val[2];
+                       / (point(0, 0) * plane(0, 0) + point(1, 0) * plane(0, 1) + point(2, 0) * plane(0, 2)));
+            depth(y, x) = point(2,0);
           }
       }
 
@@ -71,6 +75,13 @@ protected:
           cv::Vec3f normal1 = normals(y, x), normal2(plane(0, 0), plane(0, 1), plane(0, 2));
           normal1 = normal1 / cv::norm(normal1);
           normal2 = normal2 / cv::norm(normal2);
+
+          for (unsigned char i = 0; i < 3; ++i)
+            std::cout << normal1[i] << " ";
+          std::cout << " - ";
+          for (unsigned char i = 0; i < 3; ++i)
+            std::cout << normal2[i] << " ";
+          std::cout << std::endl;
 
           avg_diff += std::min(cv::norm(normal1 - normal2), cv::norm(normal1 + normal2));
         }
