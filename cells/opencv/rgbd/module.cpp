25ece2b226dc89953e8b7c23d732f42643d90ef7
Vincent Rabaud <vrabaud@willowgarage.com>
2012-06-13 17:48:26 -0500

    make normal methods available from Python

--- cells/opencv/rgbd/module.cpp
+++ cells/opencv/rgbd/module.cpp
@@ -1,5 +1,13 @@
 #include <ecto/ecto.hpp>
+#include <boost/python.hpp>
+
+#include <opencv2/rgbd/rgbd.hpp>
+
+namespace bp = boost::python;
 
 ECTO_DEFINE_MODULE(rgbd)
 {
+  bp::enum_<cv::RgbdNormals::RGBD_NORMALS_METHOD>("RgbdNormalsTypes").value("SRI",
+                                                                            cv::RgbdNormals::RGBD_NORMALS_METHOD_SRI).value(
+      "FALS", cv::RgbdNormals::RGBD_NORMALS_METHOD_FALS);
 }
