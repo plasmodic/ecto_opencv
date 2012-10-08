883535c5c6ef72e9553584c92ba179208a9a3b21
Vincent Rabaud <vrabaud@willowgarage.com>
2012-10-08 09:31:23 +0200

    deal better with the theme

--- doc/conf.py
+++ doc/conf.py
@@ -121,7 +121,7 @@ html_theme_options = { }
 html_context = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['.']
+html_theme_path = ['../../ecto/doc/source/']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
