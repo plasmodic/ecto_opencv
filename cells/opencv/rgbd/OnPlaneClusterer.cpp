1dbebeb096c2b5dcc1bd4a1f33a8019bf40e8a2d
Vincent Rabaud <vrabaud@willowgarage.com>
2013-02-05 12:33:56 +0100

    clean the PlaneClusterer and add a version that focuses on a cylinder on top

--- cells/opencv/rgbd/OnPlaneClusterer.cpp
+++ cells/opencv/rgbd/OnPlaneClusterer.cpp
@@ -41,72 +41,82 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/rgbd/rgbd.hpp>
 
-using ecto::tendrils;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-float pointDistanceSq(const cv::Vec3f& vec1, const cv::Vec3f& vec2)
-{
+float pointDistanceSq(const cv::Vec3f& vec1, const cv::Vec3f& vec2) {
   cv::Vec3f vec = vec1 - vec2;
   return vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2];
 }
 
-float pointPlaneDistance(const cv::Vec3f& vec, const cv::Vec4f& plane)
-{
-  return std::abs(vec[0] * plane[0] + vec[1] * plane[1] + vec[2] * plane[2] + plane[3]);
+float pointPlaneDistance(const cv::Vec3f& vec, const cv::Vec4f& plane) {
+  return std::abs(
+      vec[0] * plane[0] + vec[1] * plane[1] + vec[2] * plane[2] + plane[3]);
 }
 
-  /** Cell that finds the clusters touching planes detected in a given depth image
+cv::Vec3f projectPointOnPlane(const cv::Vec3f& vec, const cv::Vec4f& plane) {
+  // sol is vec + alpha*N such that it's on plane (maybe N is not normalized)
+  cv::Vec3f N(plane[0], plane[1], plane[2]);
+  float alpha = (-plane[3] - vec.dot(N)) / N.dot(N);
+  return vec + alpha * N;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/** Cell that finds the clusters touching planes detected in a given depth image
+ */
+struct OnPlaneClusterer {
+  static void declare_params(ecto::tendrils& params) {
+    params.declare(&OnPlaneClusterer::cluster_distance_, "cluster_distance",
+        "The maximum distance between a point and the cluster it belongs to.",
+        0.02);
+    params.declare(&OnPlaneClusterer::min_cluster_size_, "min_cluster_size",
+        "Min number of points for a cluster", 300);
+    params.declare(&OnPlaneClusterer::z_min_, "z_min",
+        "The amount to crop above the plane, in meters.", 0.0075);
+    params.declare(&OnPlaneClusterer::z_max_, "z_crop",
+        "The amount to keep in the z direction (meters) relative to\n"
+            "the coordinate frame defined by the pose.", 0.5);
+  }
+
+  static void declare_io(const ecto::tendrils& params, ecto::tendrils& inputs,
+      ecto::tendrils& outputs) {
+    inputs.declare(&OnPlaneClusterer::points3d_, "points3d",
+        "The 3dpoints as a cv::Mat_<cv::Vec3f>.");
+    inputs.declare(&OnPlaneClusterer::masks_, "masks",
+        "The masks for each plane.");
+    inputs.declare(&OnPlaneClusterer::planes_, "planes",
+        "The different found planes (a,b,c,d) of equation ax+by+cz+d=0.");
+
+    outputs.declare(&OnPlaneClusterer::clusters2d_, "clusters2d",
+        "For each table, a vector of 2d clusters.");
+    outputs.declare(&OnPlaneClusterer::clusters3d_, "clusters3d",
+        "For each table, a vector of 3d clusters.");
+  }
+
+  /** Get the 2d keypoints and figure out their 3D position from the depth map
+   * @param inputs
+   * @param outputs
+   * @return
    */
-  struct OnPlaneClusterer
-  {
-    static void
-    declare_params(ecto::tendrils& params)
-    {
-      params.declare(&OnPlaneClusterer::cluster_distance_, "cluster_distance", "The maximum distance between a point and the cluster it belongs to.",
-                     0.02);
-      params.declare(&OnPlaneClusterer::min_cluster_size_, "min_cluster_size", "Min number of points for a cluster", 300);
-      params.declare(&OnPlaneClusterer::table_z_filter_min_, "table_z_filter_min",
-                     "Min distance (in meters) from the table to get clusters from.", 0.01);
-      params.declare(&OnPlaneClusterer::table_z_filter_max_, "table_z_filter_max",
-                     "Max distance (in meters) from the table to get clusters from.", 0.5);
-    }
-
-    static void
-    declare_io(const tendrils& params, tendrils& inputs, tendrils& outputs)
-    {
-      inputs.declare(&OnPlaneClusterer::points3d_, "points3d", "The 3dpoints as a cv::Mat_<cv::Vec3f>.");
-      inputs.declare(&OnPlaneClusterer::mask_, "table_mask", "The mask of the different planes.");
-      inputs.declare(&OnPlaneClusterer::table_coefficients_, "table_coefficients", "The coefficients of planar surfaces.");
-
-      outputs.declare(&OnPlaneClusterer::clusters2d_, "clusters2d", "For each table, a vector of 2d clusters.");
-      outputs.declare(&OnPlaneClusterer::clusters3d_, "clusters3d", "For each table, a vector of 3d clusters.");
-    }
-
-    /** Get the 2d keypoints and figure out their 3D position from the depth map
-     * @param inputs
-     * @param outputs
-     * @return
-     */
-    int
-    process(const tendrils& inputs, const tendrils& outputs)
-    {
+  int process(const ecto::tendrils& inputs, const ecto::tendrils& outputs) {
     clusters2d_->clear();
-    clusters2d_->resize(table_coefficients_->size());
+    clusters2d_->resize(planes_->size());
     clusters3d_->clear();
-    clusters3d_->resize(table_coefficients_->size());
+    clusters3d_->resize(planes_->size());
 
     const cv::Mat_<cv::Vec3f> &points3d = *points3d_;
 
     // If an object touches a plane, its pixels also touch some pixels of the plane
     // Let's find those pixels first
-    cv::Mat_<uchar> mask_binary = (*mask_) != 255, object_seeds;
+    cv::Mat_<uchar> mask_binary = (*masks_) != 255, object_seeds;
     cv::dilate(mask_binary, object_seeds, cv::Mat());
     object_seeds = object_seeds - mask_binary;
 
     // For each potential pixel ...
     cv::Mat_<uchar> checked = mask_binary.clone();
-    for (int y = 1; y < mask_->rows - 1; ++y) {
+    for (int y = 1; y < masks_->rows - 1; ++y) {
       uchar* iter = object_seeds.ptr<uchar>(y);
-      for (int x = 1; x < mask_->cols - 1; ++x, ++iter) {
+      for (int x = 1; x < masks_->cols - 1; ++x, ++iter) {
         // Only look at pixels that are on the edge of planes
         if ((!(*iter)) || (checked(y, x)))
           continue;
@@ -115,11 +125,12 @@ float pointPlaneDistance(const cv::Vec3f& vec, const cv::Vec4f& plane)
         float plane_distance_min = std::numeric_limits<float>::max();
         for (int yy = y - 1; yy <= y + 1; ++yy)
           for (int xx = x - 1; xx <= x + 1; ++xx) {
-            if ((*mask_).at<uchar>(yy, xx) != 255) {
+            if ((*masks_).at<uchar>(yy, xx) != 255) {
               // compute the distance from the point to the plane
-              float plane_distance = pointDistanceSq(points3d(y, x), points3d(yy, xx));
+              float plane_distance = pointDistanceSq(points3d(y, x),
+                  points3d(yy, xx));
               if (plane_distance < plane_distance_min) {
-                plane_closest = (*mask_).at<uchar>(yy, xx);
+                plane_closest = (*masks_).at<uchar>(yy, xx);
                 plane_distance_min = plane_distance;
               }
             }
@@ -142,12 +153,14 @@ float pointPlaneDistance(const cv::Vec3f& vec, const cv::Vec4f& plane)
                 continue;
               // Compute the distance from that point to the original point
               const cv::Vec3f& point3d_2 = points3d(yy, xx);
-              if (pointDistanceSq(point3d_1, point3d_2) < (*cluster_distance_) * (*cluster_distance_)) {
+              if (pointDistanceSq(point3d_1, point3d_2)
+                  < (*cluster_distance_) * (*cluster_distance_)) {
                 checked(yy, xx) = 1;
                 point_list.push_back(cv::Point(xx, yy));
                 // Only add the point if it is within the plane distance boundaries
-                float dist = pointPlaneDistance(point3d_2, (*table_coefficients_)[plane_closest]);
-                if ((*table_z_filter_min_ < dist) && (dist < *table_z_filter_max_)) {
+                float dist = pointPlaneDistance(point3d_2,
+                    (*planes_)[plane_closest]);
+                if ((*z_min_ < dist) && (dist < *z_max_)) {
                   cluster2d.push_back(cv::Vec2i(xx, yy));
                   cluster3d.push_back(point3d_2);
                 }
@@ -156,7 +169,7 @@ float pointPlaneDistance(const cv::Vec3f& vec, const cv::Vec4f& plane)
           point_list.pop_front();
         }
 
-        if (cluster3d.size() < 100)
+        if (cluster3d.size() < *min_cluster_size_)
           continue;
         (*clusters2d_)[plane_closest].push_back(cluster2d);
         (*clusters3d_)[plane_closest].push_back(cluster3d);
@@ -165,25 +178,156 @@ float pointPlaneDistance(const cv::Vec3f& vec, const cv::Vec4f& plane)
 
     return ecto::OK;
   }
-  private:
-    /** Min distance between two clusters */
-    ecto::spore<float> cluster_distance_;
-    /** Min number of points for a cluster */
-    ecto::spore<int> min_cluster_size_;
-    /** Limits used when clustering points off the plane */
-    ecto::spore<float> table_z_filter_min_;
-    ecto::spore<float> table_z_filter_max_;
+private:
+  /** Min distance between two clusters */
+  ecto::spore<float> cluster_distance_;
+  /** Min number of points for a cluster */
+  ecto::spore<size_t> min_cluster_size_;
+  /** Limits used when clustering points off the plane */
+  ecto::spore<float> z_min_;
+  ecto::spore<float> z_max_;
 
-    /** The input cloud */
-    ecto::spore<cv::Mat> points3d_;
-    /** The minimum number of inliers in order to do pose matching */
-    ecto::spore<std::vector<cv::Vec4f> > table_coefficients_;
-    /** The mask of the different planes */
-    ecto::spore<cv::Mat> mask_;
-    /** The resulting clusters: for each table, return a vector of clusters */
-    ecto::spore<std::vector<std::vector<std::vector<cv::Vec2i> > > > clusters2d_;
-    ecto::spore<std::vector<std::vector<std::vector<cv::Vec3f> > > > clusters3d_;
-  };
+  /** The input cloud */
+  ecto::spore<cv::Mat> points3d_;
+  /** The minimum number of inliers in order to do pose matching */
+  ecto::spore<std::vector<cv::Vec4f> > planes_;
+  /** The mask of the different planes */
+  ecto::spore<cv::Mat> masks_;
+  /** The resulting clusters: for each table, return a vector of clusters */
+  ecto::spore<std::vector<std::vector<std::vector<cv::Vec2i> > > > clusters2d_;
+  ecto::spore<std::vector<std::vector<std::vector<cv::Vec3f> > > > clusters3d_;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/** Cell that finds the cluster of points in a cylinder on top of a plane
+ */
+struct OnPlaneClustererCylinder {
+  static void declare_params(ecto::tendrils& params) {
+    params.declare(&OnPlaneClustererCylinder::radius_max_, "radius_crop",
+        "The amount to keep in the x direction (meters) relative\n"
+            "to the coordinate frame defined by the pose.", 0.2);
+    params.declare(&OnPlaneClustererCylinder::z_min_, "z_min",
+        "The amount to crop above the plane, in meters.", 0.0075);
+    params.declare(&OnPlaneClustererCylinder::z_max_, "z_crop",
+        "The amount to keep in the z direction (meters) relative to\n"
+            "the coordinate frame defined by the pose.", 0.5);
+  }
+
+  static void declare_io(const ecto::tendrils& params, ecto::tendrils& inputs,
+      ecto::tendrils& outputs) {
+    inputs.declare(&OnPlaneClustererCylinder::points3d_, "points3d",
+        "The 3dpoints as a cv::Mat_<cv::Vec3f>.").required(true);
+    inputs.declare(&OnPlaneClustererCylinder::masks_, "masks",
+        "The masks for each plane.").required(true);
+    inputs.declare(&OnPlaneClustererCylinder::planes_, "planes",
+        "The different found planes (a,b,c,d) of equation ax+by+cz+d=0.").required(
+        true);
+    inputs.declare(&OnPlaneClustererCylinder::T_, "T",
+        "The pose translation of the focused plane.").required(true);
+
+    outputs.declare(&OnPlaneClustererCylinder::cluster2d_, "cluster2d",
+        "For each table, a vector of 2d clusters.");
+    outputs.declare(&OnPlaneClustererCylinder::mask_, "mask",
+        "For each table, a vector of 2d clusters.");
+    outputs.declare(&OnPlaneClustererCylinder::cluster3d_, "cluster3d",
+        "For each table, a vector of 3d clusters.");
+  }
+
+  /** Get the 2d keypoints and figure out their 3D position from the depth map
+   * @param inputs
+   * @param outputs
+   * @return
+   */
+  int process(const ecto::tendrils& inputs, const ecto::tendrils& outputs) {
+    cluster2d_->clear();
+    cluster3d_->clear();
+    cv::Mat_<uchar> mask = cv::Mat_<uchar>::zeros(masks_->size());
+
+    if (planes_->empty()) {
+      *mask_ = mask;
+      return ecto::OK;
+    }
+
+    // Find the closest plane to the given pose
+    cv::Vec3f T = *T_;
+    int plane_best_index = 0;
+    float distance_best = std::numeric_limits<float>::max();
+    for (size_t i = 0; i < planes_->size(); ++i) {
+      const cv::Vec4f & plane = (*planes_)[i];
+      float dist = pointPlaneDistance(T, plane);
+      if (dist < distance_best) {
+        distance_best = dist;
+        plane_best_index = i;
+      }
+    }
+    const cv::Vec4f &plane_best = (*planes_)[plane_best_index];
+
+    const cv::Mat_<cv::Vec3f> &points3d = *points3d_;
+
+    // If an object touches a plane, its pixels also touch some pixels of the plane
+    // Let's find those pixels first
+    cv::Mat_<uchar> mask_binary = (*masks_) == plane_best_index, object_seeds;
+    cv::dilate(mask_binary, object_seeds, cv::Mat());
+    object_seeds = object_seeds - mask_binary;
+
+    // For each potential pixel ...
+    cv::Mat_<uchar> checked = mask_binary.clone();
+    for (int y = 1; y < masks_->rows - 1; ++y) {
+      uchar* iter = object_seeds.ptr<uchar>(y);
+      for (int x = 1; x < masks_->cols - 1; ++x, ++iter) {
+        // Only look at pixels that are on the edge of planes
+        if ((!(*iter)) || (checked(y, x)))
+          continue;
+        // Now, proceed by region growing to find the rest of the object
+        std::list<cv::Point> point_list(1, cv::Point(x, y));
+        while (!point_list.empty()) {
+          // Look at the neighboring points
+          const cv::Point& point2d = point_list.front();
+          for (int yy = point2d.y - 1; yy <= point2d.y + 1; ++yy)
+            for (int xx = point2d.x - 1; xx <= point2d.x + 1; ++xx) {
+              if (checked(yy, xx))
+                continue;
+              const cv::Vec3f& point3d = points3d(yy, xx);
+              checked(yy, xx) = 1;
+              point_list.push_back(cv::Point(xx, yy));
+              // Only add the point if it is within the plane distance boundaries
+              float dist = pointPlaneDistance(point3d, plane_best);
+              if ((*z_min_ < dist) && (dist < *z_max_)) {
+                // Also make sure it is within the radius boundary
+                cv::Vec3f projection = projectPointOnPlane(point3d, plane_best);
+                if (cv::norm(T - projection) < *radius_max_) {
+                  cluster2d_->push_back(cv::Vec2i(xx, yy));
+                  cluster3d_->push_back(point3d);
+                  mask(xx, yy) = 255;
+                }
+              }
+            }
+          point_list.pop_front();
+        }
+      }
+    }
+
+    return ecto::OK;
+  }
+private:
+  /** The input cloud */
+  ecto::spore<cv::Mat> points3d_;
+  /** The minimum number of inliers in order to do pose matching */
+  ecto::spore<std::vector<cv::Vec4f> > planes_;
+  /** The mask of the different planes */
+  ecto::spore<cv::Mat> masks_;
+  ecto::spore<float> radius_max_, z_max_, z_min_;
+  ecto::spore<cv::Mat> T_;
+
+  /** The resulting cluster */
+  ecto::spore<std::vector<cv::Vec2i> > cluster2d_;
+  ecto::spore<std::vector<cv::Vec3f> > cluster3d_;
+  /** The mask of the 2d points that are in the cylinder */
+  ecto::spore<cv::Mat> mask_;
+};
 
 ECTO_CELL(rgbd, OnPlaneClusterer, "OnPlaneClusterer",
-          "Given a point cloud and the hull of the table, find clusters.");
+    "Given masks of planes, find clusters on top of them.");
+ECTO_CELL(rgbd, OnPlaneClustererCylinder, "OnPlaneClustererCylinder",
+    "Given the mask of a plane, find all the 3d points in a cylinder on top of it.");
